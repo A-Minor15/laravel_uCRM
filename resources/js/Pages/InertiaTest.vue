adf02f07af0835122878faddf4b5515b8ffc2123
Minoru Awamura <105.minoru@gmail.com>
2024-05-24 22:01:51 +0900

    inertiaを追加

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+This is Inertia test.<br>
+<a href="/">This is a tag.</a><br>
+<Link href="/">This is link.</Link>
+</template>
