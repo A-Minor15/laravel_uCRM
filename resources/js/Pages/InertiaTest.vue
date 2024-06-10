3798aaad2e7c335bd33e509186295736e9dfa651
Minoru Awamura <105.minoru@gmail.com>
2024-06-10 11:29:36 +0900

    Inertia Test

--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-This is Inertia test.<br>
-<a href="/">This is a tag.</a><br>
-<Link href="/">This is link.</Link>
+    This is Inertia test.<br>
+    <a href="/">This is a tag.</a><br>
+    <Link href="/">This is link.</Link><br>
+    <Link :href="route('inertia.index')">Confirm to named route.</Link>
 </template>
