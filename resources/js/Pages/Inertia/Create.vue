2765f6293923da400b430b46b51918c4da90e470
Minoru Awamura <105.minoru@gmail.com>
2024-06-14 13:49:23 +0900

    Created create method

---
+++ resources/js/Pages/Inertia/Create.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import { reactive } from 'vue'
+    import { Inertia } from '@inertiajs/inertia'
+
+    const form = reactive({
+        title: null,
+        content: null
+    })
+
+    const submitFunction = () => {
+        Inertia.post('/inertia', form)
+    }
+</script>
+
+<template>
+    <form @submit.prevent="submitFunction">
+        <input type="text" name="title" v-model="form.title"><br>
+        <input type="text" name="content" v-model="form.content">
+        <button>Submit</button>
+    </form>
+</template>
--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -1,5 +1,9 @@
 <script setup>
     import { Link } from '@inertiajs/inertia-vue3';
+    import { ref } from 'vue'
+
+    const newTitle = ref('')
+    const newContent = ref('')
 </script>
 
 <template>
@@ -8,4 +12,13 @@
     <Link href="/">This is link.</Link><br>
     <Link :href="route('inertia.index')">Confirm to named route.</Link><br>
     <Link :href="route('inertia.show', { id: 50 })">Test of route paramater.</Link>
+
+    <div class="mb-8"></div>
+    <input type="text" name="newTitle" v-model="newTitle">{{ newTitle }}<br>
+    <input type="text" name="newContent" v-model="newContent">{{ newContent }}<br>
+    <Link as="button" method="post":href="route('inertia.store')"
+    :data="{
+        title: newTitle,
+        content: newContent
+    }">Test to save of DB</Link>
 </template>
