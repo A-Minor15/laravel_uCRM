439879e12935dde2ff5674de646e0eb7400c0b74
Minoru Awamura <105.minoru@gmail.com>
2024-06-18 07:40:58 +0900

    Added flash message

--- resources/js/Pages/Inertia/index.vue
+++ resources/js/Pages/Inertia/index.vue
@@ -1,5 +1,8 @@
 <script setup>
 </script>
 <template>
-Test
+    <div v-if="$page.props.flash.message" class="bg-blue-300">
+        {{ $page.props.flash.message }}
+    </div>
+    Test
 </template>
