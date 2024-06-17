67f9082726dd3cf2017cb37d4c736c92c5b5e4e6
Minoru Awamura <105.minoru@gmail.com>
2024-06-18 08:42:43 +0900

    Tested emit

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -2,11 +2,16 @@
     import GuestLayout from '@/Layouts/GuestLayout.vue'
     import Label from '@/Components/InputLabel.vue'
     import Input from '@/Components/TextInput.vue'
+
+    const emitTest = e => console.log(e)
 </script>
 
 <template>
     <GuestLayout>
         <Label value="Title">Title</Label>
-        <Input modelValue="Initial Value"></Input>
+        <Input
+            modelValue="Initial Value"
+            @update:modelValue="emitTest">
+        </Input>
     </GuestLayout>
 </template>
