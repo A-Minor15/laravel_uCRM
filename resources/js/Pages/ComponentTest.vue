771220af74071d5bb8eba80dbf5d11152b4cf74f
Minoru Awamura <105.minoru@gmail.com>
2024-06-18 08:31:41 +0900

    Used Component

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import GuestLayout from '@/Layouts/GuestLayout.vue'
+    import Label from '@/Components/InputLabel.vue'
+    import Input from '@/Components/TextInput.vue'
+</script>
+
+<template>
+    <GuestLayout>
+        <Label value="Title">Title</Label>
+        <Input modelValue="Initial Value"></Input>
+    </GuestLayout>
+</template>
